103454245e5c442a38c52f12719b599fe41cfc21
dpw6 <dpw6@sfu.ca>
2022-01-09 00:12:59 -0800

    added decimal point functionality

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ function reset(disp_msg){
     a_active = true;
     display.textContent = disp_msg;
     operation_active = false;
+    dec_active = false;
+    dec_place = 1;
 }
 
 const display = document.querySelector('.display');
@@ -49,11 +51,21 @@ let b = 0;
 let a_active = true;
 let operation_active = false;
 let curr_operation = -1;
+let dec_active = false;
+let dec_place = 1;
 
 const ac_btn = document.querySelector('.ac');
 ac_btn.addEventListener('click', () => {
     reset(0);
 });
+
+const dec_btn = document.querySelector('.decimal');
+dec_btn.addEventListener('click', () => {
+    if (!dec_active){
+        display.textContent = display.textContent + ".";
+        dec_active = true;
+    }
+})
 
 const p_m = document.querySelector('.p-m');
 p_m.addEventListener('click', () => {
@@ -72,151 +84,301 @@ p_m.addEventListener('click', () => {
 
 const nine = document.querySelector('.nine');
 nine.addEventListener('click', () => {
+    let z = 9;
     if (operation_active){
         reset(9);
     }
     else if (a_active){
-        a = (a * 10) + 9;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 9;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const eight = document.querySelector('.eight');
 eight.addEventListener('click', () => {
+    let z = 8;
     if (operation_active){
         reset(8);
     }
     else if (a_active){
-        a = (a * 10) + 8;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 8;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const seven = document.querySelector('.seven');
 seven.addEventListener('click', () => {
+    let z = 7;
     if (operation_active){
         reset(7);
     }
     else if (a_active){
-        a = (a * 10) + 7;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 7;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const six = document.querySelector('.six');
 six.addEventListener('click', () => {
+    let z = 6;
     if (operation_active){
         reset(6);
     }
     else if (a_active){
-        a = (a * 10) + 6;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 6;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const five = document.querySelector('.five');
 five.addEventListener('click', () => {
+    let z = 5;
     if (operation_active){
         reset(5);
     }
     else if (a_active){
-        a = (a * 10) + 5;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 5;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const four = document.querySelector('.four');
 four.addEventListener('click', () => {
+    let z = 4;
     if (operation_active){
         reset(4);
     }
     else if (a_active){
-        a = (a * 10) + 4;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 4;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const three = document.querySelector('.three');
 three.addEventListener('click', () => {
+    let z = 3;
     if (operation_active){
         reset(3);
     }
     else if (a_active){
-        a = (a * 10) + 3;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 3;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const two = document.querySelector('.two');
 two.addEventListener('click', () => {
+    let z = 2;
     if (operation_active){
         reset(2);
     }
     else if (a_active){
-        a = (a * 10) + 2;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 2;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const one = document.querySelector('.one');
 one.addEventListener('click', () => {
+    let z = 1;
     if (operation_active){
         reset(1);
     }
     else if (a_active){
-        a = (a * 10) + 1;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = (b * 10) + 1;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
 const zero = document.querySelector('.zero-key');
 zero.addEventListener('click', () => {
+    let z = 0;
     if (operation_active){
         reset(0);
     }
     else if (a_active){
-        a = a * 10;
-        display.textContent = a;
+        if (!dec_active){
+            a = (a * 10) + z;
+            display.textContent = a;
+        }
+        else{
+            a = a + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        }
     }
     else{
-        b = b * 10;
-        display.textContent = b;
+        if (!dec_active){
+            b = (b * 10) + z;
+            display.textContent = b;
+        }
+        else{
+            b = b + z/(10**dec_place);
+            dec_place ++;
+            display.textContent = display.textContent + z.toString();
+        } 
     }
 });
 
@@ -240,6 +402,8 @@ plus.addEventListener('click', () => {
     curr_operation = 0;
     a_active = false;
     operation_active = false;
+    dec_active = false;
+    dec_place = 1;
     console.log("a = " + a);
     console.log("b = " + b);
 });
@@ -248,16 +412,31 @@ const sub = document.querySelector('.subtract');
 sub.addEventListener('click', () => {
     curr_operation = 1;
     a_active = false;
+    operation_active = false;
+    dec_active = false;
+    dec_place = 1;
+    console.log("a = " + a);
+    console.log("b = " + b);
 });
 
 const mult = document.querySelector('.multiply');
 mult.addEventListener('click', () => {
     curr_operation = 2;
     a_active = false;
+    operation_active = false;
+    dec_active = false;
+    dec_place = 1;
+    console.log("a = " + a);
+    console.log("b = " + b);
 });
 
 const divide_btn = document.querySelector('.divide');
 divide_btn.addEventListener('click', () => {
     curr_operation = 3;
     a_active = false;
+    operation_active = false;
+    dec_active = false;
+    dec_place = 1;
+    console.log("a = " + a);
+    console.log("b = " + b);
 });
